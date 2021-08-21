5f33fdcc082a7cf1a29203aa2c8c1524f3c403ad
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-08-21 17:39:06 +0800

    Update

--- frontend/src/Components/Admissions/List.tsx
+++ frontend/src/Components/Admissions/List.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
+import { v4 } from 'uuid';
 import { statuses } from '../../constants';
 import { AdmissionContract } from '../../Contracts/admission.contract';
 import { UserContract } from '../../Contracts/user.contract';
 import { handleError, Asker } from '../../helpers';
-import { useURL } from '../../hooks';
+import { useNullable, useURL } from '../../hooks';
 import { State } from '../../Libraries/State';
 import { admissionService } from '../../Services/admission.service';
 import { userService } from '../../Services/user.service';
@@ -14,8 +16,16 @@ import Tooltip from '../Shared/Tooltip';
 
 type Props = {};
 
+type UserInput = {
+	payment_status: 'Not Paid' | 'Partially Paid' | 'Fully Paid';
+};
+
+const updatePaymentModalRef = v4();
+
 const List: FC<Props> = (props) => {
 	const { data: items, isFetching: loading, isError, error, refetch } = useQuery('admissions', () => admissionService.fetch());
+	const { register: registerUser, handleSubmit: handleSubmitUser, reset: resetUser, setValue: setValueUser } = useForm<UserInput>();
+	const [student, setStudent] = useNullable<number>();
 
 	const url = useURL();
 
@@ -32,6 +42,20 @@ const List: FC<Props> = (props) => {
 	if (isError) {
 		handleError(error);
 	}
+
+	const submitUser = async (data: UserInput) => {
+		try {
+			await userService.update(student, data);
+			toastr.success('Student payment updated succesfully.');
+			refetch();
+		} catch (error) {
+			handleError(error);
+		} finally {
+			$(`#${updatePaymentModalRef}`).modal('hide');
+			setStudent(null);
+			resetUser();
+		}
+	};
 
 	const deleteItem = async (id: any) => {
 		try {
@@ -94,6 +118,20 @@ const List: FC<Props> = (props) => {
 						<Link to={url(`${admission.id}/edit`)} className='btn btn-warning btn-sm mx-1' data-tip='Edit'>
 							<i className='fas fa-edit'></i>
 						</Link>
+						<button
+							className='btn btn-secondary btn-sm mx-1'
+							data-tip='Update Payment'
+							onClick={(e) => {
+								e.preventDefault();
+								const modal = $(`#${updatePaymentModalRef}`);
+								if (modal.length > 0) {
+									setStudent(admission.student?.id!);
+									setValueUser('payment_status', admission.student?.payment_status!);
+									modal.modal('toggle');
+								}
+							}}>
+							<i className='fas fa-money-bill'></i>
+						</button>
 						<button
 							className={`btn btn-${admission.student?.active ? 'danger' : 'info'} btn-sm mx-1`}
 							onClick={async (e) => {
@@ -163,6 +201,38 @@ const List: FC<Props> = (props) => {
 					</>
 				}
 			/>
+			<div id={updatePaymentModalRef} className='modal fade' tabIndex={-1}>
+				<div className='modal-dialog modal-dialog-centered modal-lg'>
+					<div className='modal-content'>
+						<div className='modal-header'>
+							<h5 className='modal-title'>Update Payment</h5>
+							<button type='button' className='close' data-dismiss='modal'>
+								<span aria-hidden='true'>&times;</span>
+							</button>
+						</div>
+						<form onSubmit={handleSubmitUser(submitUser)}>
+							<div className='modal-body'>
+								<div className='form-group'>
+									<label htmlFor='payment_status'>Payment Status</label>
+									<select {...registerUser('payment_status')} id='payment_status' className='form-control'>
+										<option value='Not Paid'>Not Paid</option>
+										<option value='Partially Paid'>Partially Paid</option>
+										<option value='Fully Paid'>Fully Paid</option>
+									</select>
+								</div>
+							</div>
+							<div className='modal-footer'>
+								<button type='submit' className='btn btn-primary btn-sm'>
+									Submit
+								</button>
+								<button type='button' className='btn btn-secondary btn-sm' data-dismiss='modal'>
+									Close
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 			<Tooltip />
 		</>
 	);
